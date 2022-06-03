8cfb58b9bac6893687935ebade94652c4ceab79f
Loris <lorisj@umich.edu>
2022-06-02 21:25:18 -0400

    removed old linalg utilities

--- src/linalg_util.py
+++ src/linalg_util.py
@@ -1,19 +1,4 @@
 import sympy as sp
-# Diagonalizes matrix mat_in into a list of eigenvectors/eigenvalues that are orthoganal to each other.
-# arguments: 
-#               mat_in: matrix input (must be hermitian)
-#
-#
-# returns:      List diag
-#                    diag = [ [a_i, |i>] ]_i  where for all i:  a_i is a complex number, |i> is a vector in C^n
-#                           each entry in diag = [eigenvalue_i, eigenvector_i]
-#               Note that repeated eigenvalues are okay
-
-def spectral_decomp(mat_in):
-    assert(mat_in.H == mat_in)
-    
-    return diag
-
 
 # Given a list of the new orientation of bits, calculate the new index version of index_in.
 # Requires: list_in to be complete!
